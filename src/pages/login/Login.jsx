01c0e93afc64aa40d37f0ca00145938a8aef5ada
Anamemem <anamememcyril540@gmail.com>
2021-10-21 15:00:24 +0100

    french

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,31 +2,46 @@ import { Link } from "react-router-dom"
 import "./login.css"
 import {auth} from '../../firebase'
 import {signInWithEmailAndPassword} from "firebase/auth"
+import { useHistory } from "react-router-dom";
 
 export default function Login() {
+    let history = useHistory();
     
-    const login = ()=>{
-      
+    const login = (e)=>{
+        
+        e.preventDefault()
         const form = document.forms['login']
         const email = form.email.value
         const password =form.password.value
         signInWithEmailAndPassword(auth,email,password).then((es)=>{
-            console.log('done');
+            console.log(es);
+            history.push("/home");
+        }).catch(e=>{
+            console.log(e)
         })
-    }    
-    return (
-        <div className="login">
-            <span className="loginTitle">Login</span>
-            <form name='login' className="loginForm">
-                <label >Email</label>
-                <input type="text" name='email' className="loginInput" placeholder="Enter your Email......."/>
-                <label >Password</label>
-                <input type="password" name='password' className="loginInput" placeholder="Enter your Password......."/>
-       <button onClick={login} className="loginButton">Login</button>
-            </form>
-            <button onClick={login} className="loginRegisterButton">
-                <Link className="link" to="/register">Register</Link>
-            </button>
-        </div>
-    )
+    } 
+    // function HomeButton() {
+        
+      
+    //     function handleClick() {
+          
+    //     }
+      
+        return (
+            <div className="login">
+                <span className="loginTitle">Login</span>
+                <form  name='login' className="loginForm">
+                    <label >Email</label>
+                    <input type="text" name='email' className="loginInput" placeholder="Enter your Email......."/>
+                    <label >Password</label>
+                    <input type="password" name='password' className="loginInput" placeholder="Enter your Password......."/>
+           <button onClick={login} className="loginButton">Login</button>
+                </form>
+                <button onClick={login} className="loginRegisterButton">
+                    <Link className="link" to="/register">Register</Link>
+                </button>
+            </div>
+        )
+        
+   
 }
